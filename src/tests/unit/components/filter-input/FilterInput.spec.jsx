012bd0b16b44c830f545b7e19c639050760a179e
Serhii Zakharchuk <zakharchuksergij@gmail.com>
2025-04-15 23:37:41 +0300

    Update FilterInput component

--- src/tests/unit/components/filter-input/FilterInput.spec.jsx
+++ src/tests/unit/components/filter-input/FilterInput.spec.jsx
@@ -1,27 +1,45 @@
 import { render, fireEvent } from '@testing-library/react'
 import FilterInput from '~/components/filter-input/FilterInput'
+import { vi } from 'vitest'
 
 describe('FilterInput', () => {
   it('renders the input field', () => {
-    const { getByRole } = render(<FilterInput />)
+    const { getByRole } = render(<FilterInput onChange={() => {}} />)
     expect(getByRole('textbox')).toBeInTheDocument()
   })
 
-  it('calls the onChange function when text is entered', () => {
-    const handleChange = vi.fn()
-    const { getByRole } = render(<FilterInput onChange={handleChange} />)
+  it('renders typed text correctly', () => {
+    const { getByRole } = render(
+      <FilterInput onChange={() => {}} value='hello' />
+    )
     const input = getByRole('textbox')
-    fireEvent.change(input, { target: { value: 'test' } })
-    expect(handleChange).toHaveBeenCalledWith('test')
+    expect(input.value).toBe('hello')
   })
 
-  it('clears the input when the clear button is clicked', () => {
+  it('deletes typed text when delete button is clicked', () => {
     const handleChange = vi.fn()
     const { getByTestId } = render(
-      <FilterInput onChange={handleChange} value='test' />
+      <FilterInput value='hello' onChange={handleChange} />
     )
     const clearButton = getByTestId('clear-button')
     fireEvent.click(clearButton)
     expect(handleChange).toHaveBeenCalledWith('')
+  })
+
+  it('calls updateFilter function on search icon (if used as button)', () => {
+    const handleChange = vi.fn()
+    render(<FilterInput value='' onChange={handleChange} />)
+  })
+
+  it('calls updateFilter function when Enter is pressed', () => {
+    const handleChange = vi.fn()
+    const { getByRole } = render(
+      <FilterInput value='typed' onChange={handleChange} />
+    )
+    const input = getByRole('textbox')
+
+    fireEvent.keyDown(input, { key: 'Enter', code: 'Enter', charCode: 13 })
+
+    expect(handleChange).not.toHaveBeenCalledWith('typed')
   })
 })
