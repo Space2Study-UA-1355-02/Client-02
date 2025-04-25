20b6e49a4cebd084e842c4024a29cf18a6c8b215
Don-de-padlo <55998698+Don-de-padlo@users.noreply.github.com>
2025-04-25 13:39:27 +0300

    add language step

--- src/containers/tutor-home-page/language-step/LanguageStep.jsx
+++ src/containers/tutor-home-page/language-step/LanguageStep.jsx
@@ -1,19 +1,48 @@
+import React, { useState } from 'react'
 import Box from '@mui/material/Box'
 
 import { styles } from '~/containers/tutor-home-page/language-step/LanguageStep.styles'
-import img from '~/assets/img/tutor-home-page/become-tutor/languages.svg'
-
+import Typography from '@mui/material/Typography'
+import TextField from '@mui/material/TextField'
+import MenuItem from '@mui/material/MenuItem'
+import { languagesMock } from '~/containers/tutor-home-page/subjects-step/constants'
 const LanguageStep = ({ btnsBox }) => {
+  const [form, setForm] = useState({
+    nativeLang: 'Ukrainian'
+  })
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setForm((prev) => ({
+      ...prev,
+      [name]: value
+    }))
+  }
+
   return (
-    <Box sx={styles.container}>
-      <Box sx={styles.imgContainer}>
-        <Box component='img' src={img} sx={styles.img} />
+    <>
+      <Typography variant='body1'>
+        Velit officia consequat duis enim velit mollit. Other categories you can
+        add in your account settings later.
+      </Typography>
+      <Box sx={styles.inputs}>
+        <TextField
+          fullWidth
+          label='Your native language'
+          name='nativeLang'
+          onChange={handleChange}
+          select
+          value={form.nativeLang}
+        >
+          {languagesMock.map((lang) => (
+            <MenuItem key={lang.name} value={lang.name}>
+              {lang.name}
+            </MenuItem>
+          ))}
+        </TextField>
       </Box>
-      <Box sx={styles.rigthBox}>
-        <Box>Language here</Box>
-        {btnsBox}
-      </Box>
-    </Box>
+      {btnsBox}
+    </>
   )
 }
 
