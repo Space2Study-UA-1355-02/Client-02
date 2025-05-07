f4d48d98c9c7f86c46c42f1f73244ad6f1853101
ruslansymonenko <symonenkors@gmail.com>
2025-05-07 14:09:33 +0300

    Close (#90): added no result found component to category page

--- src/pages/categories/Categories.tsx
+++ src/pages/categories/Categories.tsx
@@ -1,7 +1,23 @@
 import PageWrapper from '~/components/page-wrapper/PageWrapper'
+import NotFoundResults from '~/components/not-found-results/NotFoundResults'
 
 const Categories = () => {
-  return <PageWrapper>Categories</PageWrapper>
+  const categories = []
+
+  return (
+    <PageWrapper>
+      {categories.length ? (
+        <div>Category</div>
+      ) : (
+        <NotFoundResults
+          buttonText={'Request a new category'}
+          description={
+            "We couldn't find what you were searching for. Please try again or suggest a new category that you were looking for."
+          }
+        />
+      )}
+    </PageWrapper>
+  )
 }
 
 export default Categories
