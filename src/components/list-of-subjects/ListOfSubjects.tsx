31d2584cdba1179046c3d2f8b32ad556749ba6b6
ruslansymonenko <symonenkors@gmail.com>
2025-05-07 16:02:24 +0300

    added list of subjects block

---
+++ src/components/list-of-subjects/ListOfSubjects.tsx
@@ -0,0 +1,51 @@
+import { FC } from 'react'
+import { Grid, Box } from '@mui/material'
+import AppButton from '~/components/app-button/AppButton'
+import { styles } from '~/components/list-of-subjects/ListOfSubjects.styles'
+import { ButtonVariantEnum, SubjectInterface } from '~/types'
+import { ListOfSubjectsCard } from '~/components/list-of-subjects-card/ListOfSubjectsCard'
+import NotFoundResults from '~/components/not-found-results/NotFoundResults'
+
+interface ListOfSubjectProps {
+  items: SubjectInterface[]
+  onClick?: () => void
+}
+
+export const ListOfSubjects: FC<ListOfSubjectProps> = ({ onClick, items }) => {
+  return (
+    <Box sx={styles.container}>
+      {items.length ? (
+        <>
+          <Grid container spacing={2}>
+            {items.map((item) => (
+              <Grid item key={item._id} lg={3} md={4} sm={6} xs={12}>
+                <ListOfSubjectsCard
+                  _id={item._id}
+                  category={item.category}
+                  createdAt={item.createdAt}
+                  name={item.name}
+                  totalOffers={item.totalOffers}
+                  updatedAt={item.updatedAt}
+                />
+              </Grid>
+            ))}
+          </Grid>
+
+          <AppButton
+            onClick={onClick}
+            sx={styles.button}
+            variant={ButtonVariantEnum.Tonal}
+          >
+            View more
+          </AppButton>
+        </>
+      ) : (
+        <NotFoundResults
+          description={
+            "We couldn't find what you were searching for. Please try again or suggest a new category that you were looking for."
+          }
+        />
+      )}
+    </Box>
+  )
+}
