94595d819629423c93304d6e29230f27fc12faaf
ruslansymonenko <symonenkors@gmail.com>
2025-05-09 19:26:13 +0300

    created block which allows to student or tutor create request

---
+++ src/components/create-request-block/CreateRequest.styles.ts
@@ -0,0 +1,23 @@
+export const styles = {
+  container: {
+    px: 8,
+    py: 14,
+    borderRadius: 4,
+    display: 'flex',
+    alignItems: 'center',
+    backgroundColor: '#d2f2f3'
+  },
+  content: {
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  leftSide: {
+    flex: 1,
+    maxWidth: '40%'
+  },
+  title: { fontWeight: 'bold' },
+  subTitle: { my: 4 },
+  image: { height: '100%', ml: 2 }
+}
