71cdd6c7f5b2d6739d975e1cd52e12794c91005e
Ruslan Symonenko <99801898+ruslansymonenko@users.noreply.github.com>
2025-05-12 20:51:32 +0300

    Closed (#90): added no result found component to category page (#40)
    Co-authored-by: Roman Kryzhanivsky <77616134+Kryzhanivsky@users.noreply.github.com>

--- src/pages/categories/Categories.tsx
+++ src/pages/categories/Categories.tsx
@@ -1,12 +1,22 @@
 import PageWrapper from '~/components/page-wrapper/PageWrapper'
+import NotFoundResults from '~/components/not-found-results/NotFoundResults'
 import SearchCategories from '~/containers/student-categories-page/search-categories/SearchCategories'
 
 const Categories = () => {
+  const categories = []
+
   return (
     <PageWrapper>
       <SearchCategories onSearch={() => {}} />
-    </PageWrapper>
-  )
-}
+      {categories.length ? (
+        <div>Category</div>
+      ) : (
+        <NotFoundResults
+          buttonText={'Request a new category'}
+          description={
+            "We couldn't find what you were searching for. Please try again or suggest a new category that you were looking for."
+          }
+        />
+      )}
 
 export default Categories
