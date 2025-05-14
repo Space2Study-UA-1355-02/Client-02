003ca11ae9c7c8ae3997daa5679e1b9f23cf7c78
Olena <kusaeva@gmail.com>
2025-05-14 10:31:21 +0200

    Add list grid switcher buttons

---
+++ src/components/listGridSwitch/ListGridSwitch.jsx
@@ -0,0 +1,29 @@
+import { ToggleButtonGroup, ToggleButton } from '@mui/material'
+import ListIcon from '@mui/icons-material/List'
+import GridViewIcon from '@mui/icons-material/GridView'
+
+const ListGridSwitch = ({ isGrid, setIsGrid }) => {
+  const handleChange = (_, newValue) => {
+    if (newValue !== null) {
+      setIsGrid(newValue)
+    }
+  }
+  return (
+    <ToggleButtonGroup
+      aria-label='View toggle'
+      exclusive
+      onChange={handleChange}
+      size='small'
+      value={isGrid}
+    >
+      <ToggleButton aria-label='List view' value={false}>
+        <ListIcon sx={{ mr: 1 }} />
+      </ToggleButton>
+      <ToggleButton aria-label='Grid view' value>
+        <GridViewIcon sx={{ mr: 1 }} />
+      </ToggleButton>
+    </ToggleButtonGroup>
+  )
+}
+
+export default ListGridSwitch
