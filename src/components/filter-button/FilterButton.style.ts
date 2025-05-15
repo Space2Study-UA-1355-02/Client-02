22a53406229e34460d6d9fa15b6830c59a2558b9
ruslansymonenko <symonenkors@gmail.com>
2025-05-15 16:59:59 +0300

    added filter button component

---
+++ src/components/filter-button/FilterButton.style.ts
@@ -0,0 +1,18 @@
+export const styles = {
+  container: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  counter: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    background: '#78909C',
+    borderRadius: '50%',
+    width: '10px',
+    height: '10px',
+    p: 1,
+    color: 'white',
+    fontWeight: 'bold'
+  }
+}
